59c7516d4fd01de01c9a6ea9f50c8dd700ca23de
githubverycool <githubemail@sina.com>
2015-07-30 02:31:18 +0800

    1.创建cxf-ws-util新项目

---
+++ src/main/java/com/server/IUserService.java
@@ -0,0 +1,12 @@
+package com.server;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public interface IUserService {
+
+	@WebMethod
+	public String getSuccess(int i);
+	
+}
---
+++ src/main/java/com/server/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.server.impl;
+
+import javax.jws.WebService;
+import javax.swing.Spring;
+
+import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
+
+import com.server.IUserService;
+
+@WebService(endpointInterface="com.server.IUserService",serviceName="UserService",portName="UserServicePort")
+public class UserServiceImpl  implements IUserService {
+
+	@Override
+	public String getSuccess(int i) {
+		return "success:" + i;
+	}
+	
+    public static void main(String[] args) {  
+        System.out.println("Server is starting...");  
+        javax.xml.ws.Endpoint.publish("http://localhost:8080/cxf-ws-util/userservice", new UserServiceImpl());
+        System.out.println("Server is started...");  
+    }  
+
+}
